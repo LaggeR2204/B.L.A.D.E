df75232e58c1905b66c1e96c67ec45856e15137d
AlphaW <minhduc9111999@gmail.com>
2019-12-06 02:13:40 +0700

    duc

---
+++ BLADE/DrawingWaveSound.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace BLADE
+{
+    class DrawingWaveSound
+    {
+        public delegate void NotifySampleHandler(object sender, SampleEventArgs e);
+        public event NotifySampleHandler NotifySample;
+        private WasapiLoopbackCapture _audioSource;
+        private BufferedWaveProvider _provider;
+        private WaveOut _player;
+        public WasapiLoopbackCapture AudioSource
+        { 
+            get => _audioSource;
+            set
+            {
+                _audioSource = value;
+               
+            }
+        }
+        public BufferedWaveProvider Provider { get => _provider; set => _provider = value; }
+        public WaveOut Player { get => _player; set => _player = value; }
+        public int Count { get => _count; set => _count = value; }
+        public int Speed { get => _speed; }
+        public float LeftMax { get => _leftMax; set => _leftMax = value; }
+        public float RightMax { get => _rightMax; set => _rightMax = value; }
+        public object SampleObject { get => _sampleObject; set => _sampleObject = value; }
+        public NotifyingSampleProvider Notify { get => _notify; set => _notify = value; }
+
+        private int _count;
+        private const int _speed = 1;
+        private float _leftMax, _rightMax;
+        private object _sampleObject;
+        private NotifyingSampleProvider _notify;
+
+        public DrawingWaveSound()
+        {
+            _audioSource = new WasapiLoopbackCapture();
+            _audioSource.DataAvailable += _audioSource_DataAvailable;
+            _provider = new BufferedWaveProvider(new WaveFormat());
+            _player = new WaveOut();
+            _sampleObject = new object();
+            _notify = new NotifyingSampleProvider(_provider.ToSampleProvider());
+            _notify.Sample += _notify_Sample;
+        }
+
+        private void _notify_Sample(object sender, SampleEventArgs e)
+        {
+            if (NotifySample != null)
+                NotifySample(this, e);
+        }
+
+        private void _audioSource_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            BufferPlay(e.Buffer);
+        }
+        private void BufferPlay(byte[] recv)
+        {
+            if(recv.Length > 0)
+            {
+                _provider.AddSamples(recv, 0, recv.Length);
+            }
+        }
+        private void StartAudio()
+        {
+            _audioSource.StartRecording();
+            _player.Init(_notify);
+            _player.Play();
+        }
+        private void StopAudio()
+        {
+            if (_player == null) return;
+            _player.Dispose();
+            _player.Stop();
+            _provider.ClearBuffer();
+        }
+
+
+    }
+}
--- BLADE/ucNowPlaying.cs
+++ BLADE/ucNowPlaying.cs
@@ -7,14 +7,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NAudio_Plotting;
 
 namespace BLADE
 {
     public partial class ucNowPlaying : UserControl
     {
+        StereoAudioWavePainter stereo;
+        DrawingWaveSound _drawtool;
         public ucNowPlaying()
         {
             InitializeComponent();
+            stereo = new StereoAudioWavePainter();
+            stereo.Location = new Point(100, 100);
+            stereo.Size = new Size(800, 400);
+            stereo.DrawMode = StereoAudioWavePainter.DisplayMode.Line;
+            stereo.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            stereo.Gradient = true;
+            stereo.GradientColor = Color.FromArgb(0, 190, 190);
+            stereo.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            stereo.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+            stereo.Plot = StereoAudioWavePainter.Plotmode.PlusMinus;
+            stereo.ShowMidLine = false;
+            stereo.Enabled = true;
+            stereo.BackColor = Color.FromArgb(0, 190, 190);
+            stereo.Show();
+            _drawtool = new DrawingWaveSound();
+            _drawtool.NotifySample += _drawtool_NotifySample;
+        }
+
+        private void _drawtool_NotifySample(object sender, NAudio.Wave.SampleEventArgs e)
+        {
+            lock (_drawtool.SampleObject)
+            {
+                if(_drawtool.Count >= _drawtool.Speed)
+                {
+                    stereo.AddLeftRight(_drawtool.LeftMax, _drawtool.RightMax);
+                    _drawtool.LeftMax = 0;
+                    _drawtool.RightMax = 0;
+                    _drawtool.Count = 0;
+                }
+                else
+                {
+                    if (Math.Abs(e.Left) + Math.Abs(e.Right) > Math.Abs(_drawtool.LeftMax) + Math.Abs(_drawtool.RightMax))
+                    {
+                        _drawtool.LeftMax = e.Left;
+                        _drawtool.RightMax = e.Right;
+                    }
+                    _drawtool.Count++;
+                }
+            }
         }
     }
 }
