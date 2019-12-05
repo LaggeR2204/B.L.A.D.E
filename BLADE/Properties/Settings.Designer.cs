d9c902898762b00347c669cdcacc4d04519954c3
quocan22 <quocan.omg@gmail.com>
2019-12-05 15:02:45 +0700

    some edit

--- BLADE/Properties/Settings.Designer.cs
+++ BLADE/Properties/Settings.Designer.cs
@@ -46,5 +46,17 @@ namespace BLADE.Properties {
                 this["Password"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool RememberChecked {
+            get {
+                return ((bool)(this["RememberChecked"]));
+            }
+            set {
+                this["RememberChecked"] = value;
+            }
+        }
     }
 }
