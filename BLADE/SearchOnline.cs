c9c89312d3402b8b52b180d9fab61cbd6bc71f15
Ryuujin <longhairbees@gmail.com>
2019-12-26 11:29:45 +0700

    Đổi wmp.dll thay vì dùng mặc định của Sys32

--- BLADE/SearchOnline.cs
+++ BLADE/SearchOnline.cs
@@ -9,7 +9,6 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using AxWMPLib;
 using WMPLib;
 using xNet;
 using BLADE.xDialog;
